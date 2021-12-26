14fd1537e8a53f966d0144b2ec934bcc24717776
flofriday <flohacksfriday@gmail.com>
2021-12-26 13:39:19 +0100

    fix python linting to autodetect (single) python files this may not be enough as we probalbly still need to also auto-detect pacakages so that pylint can handle them differently.

--- backend/glint_server/file.py
+++ backend/glint_server/file.py
@@ -5,11 +5,13 @@ import urllib.parse
 
 def create_project_folder(name: str) -> str:
     path = app.config["LINT_DIR"] + name
-    os.makedirs(path) # comment this out to test frontend/backend connection without writing folders everywhere
+    os.makedirs(
+        path
+    )  # comment this out to test frontend/backend connection without writing folders everywhere
     return path
 
 
 def save_file(file_name: str, content: str):
-    #return # I have this in here to test frontend/backend connection without writing files everywhere
+    # return # I have this in here to test frontend/backend connection without writing files everywhere
     with open(urllib.parse.unquote(file_name), "w") as f:
         f.write(content)
