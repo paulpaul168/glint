e6e8a15161b99a8c28369d4853f76ee0b17beedb
paulpaul168 <github@paulhoeller.at>
2021-12-26 14:02:32 +0100

    fix merge conflict

--- backend/glint_server/file.py
+++ backend/glint_server/file.py
@@ -13,13 +13,8 @@ def create_project_folder(name: str) -> str:
 
 
 def save_file(file_name: str, content: str):
-<<<<<<< HEAD
     print(urllib.parse.unquote(file_name))
     # return # I have this in here to test frontend/backend connection without writing files everywhere
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
     with open(urllib.parse.unquote(file_name), "w+") as f:
-=======
-    # return # I have this in here to test frontend/backend connection without writing files everywhere
-    with open(urllib.parse.unquote(file_name), "w") as f:
->>>>>>> 14fd1537e8a53f966d0144b2ec934bcc24717776
         f.write(content)
