365720e45cd07a98700e63439e2d890900c54eff
paulpaul168 <github@paulhoeller.at>
2021-12-26 13:54:51 +0100

    fix path during creation, create dir if does not exist

--- backend/glint_server/views.py
+++ backend/glint_server/views.py
@@ -5,7 +5,8 @@ import urllib.parse
 from glint_server.linter_collection import lint_project
 from glint_server.file import save_file, create_project_folder
 
-import time #sometimes needed to test frontend behavior for slow answers
+import time  # sometimes needed to test frontend behavior for slow answers
+
 
 @app.get("/")
 def home():
@@ -21,24 +22,19 @@ def create_project():
     post_content = request.json
     project_path = str(create_project_folder(post_content["name"]))
     for file in post_content["files"]:
-        save_file(file["path"], file["content"])
+        save_file(project_path + "/" + file["path"], file["content"])
         print(file)
     project_path = urllib.parse.quote(project_path)
     data = {
         "name": post_content["name"],
         "projectId": project_path,
         "projectUrl": app.config["HOST"] + "/api/projects" + project_path,
-        "sourcesUrl": app.config["HOST"]
-        + "/api/projects"
-        + project_path
-        + "/sources",
-        "lintUrl": app.config["HOST"]
-        + "/api/projects"
-        + project_path
-        + "/lint",
+        "sourcesUrl": app.config["HOST"] + "/api/projects" + project_path + "/sources",
+        "lintUrl": app.config["HOST"] + "/api/projects" + project_path + "/lint",
     }
-    #time.sleep(1)
+    # time.sleep(1)
     return prepareResponse(data)
+
 
 # needed because browser sends "options" request to same link before sending POST to check if CORS is allowed and I don't see another way to respond to the OPTIONS http request (@app.options doesn't exist)
 @app.after_request
@@ -47,6 +43,7 @@ def return_allow_cors(resp):
     resp.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
     resp.headers["Access-Control-Allow-Headers"] = "Content-Type"
     return resp
+
 
 @app.get("/api/projects/<project_id>/lint")
 def get_lint(project_id):
