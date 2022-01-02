f82865f81138ca719440626a41733718a5e3efa3
paulpaul168 <github@paulhoeller.at>
2022-01-02 14:12:17 +0100

    write processing to file, better variable name

--- backend/glint_server/threading.py
+++ backend/glint_server/threading.py
@@ -2,17 +2,20 @@ from glint_server.linter_collection import lint_project
 from glint_server.file import save_file
 from glint_server import app
 import urllib, threading, json
+import glint_server.file as gfile
+from glint_server.linter_collection.lint import lint_project_processing
 
 
 def do_lint(project_id: str, linters: dict) -> None:
-    x = threading.Thread(
+    gfile.save_file(project_id + "/lint.glint", json.dumps(lint_project_processing()))
+    thread = threading.Thread(
         target=lint_thread,
         args=(
             project_id,
             linters,
         ),
     )
-    x.start()
+    thread.start()
 
 
 def lint_thread(project_id: str, linters: dict) -> None:
